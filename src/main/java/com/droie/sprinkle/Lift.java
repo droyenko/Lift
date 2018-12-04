e3f925db0844d2fe083b16e65202497f04fa6cb8
Dmytro Roienko <droie@softserveinc.com>
2018-12-04 17:49:08 +0200

    method now returns array of levels that elevator is stops on for better testing performance

--- src/main/java/com/droie/sprinkle/Lift.java
+++ src/main/java/com/droie/sprinkle/Lift.java
@@ -1,11 +1,21 @@
 package com.droie.sprinkle;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
 public class Lift {
     public static int[] theLift(final int[][] queues) {
+        Set<Integer> orderUp = new HashSet<>(Arrays.asList(0));
+        TreeSet<Integer> orderDown = new TreeSet<>();
         for (int i = 0; i < queues.length; i++) {
             int[] queue = queues[i];
             for (int levelToGo : queue) {
                 if (levelToGo > i) {
+                    orderUp.add(i);
+                    orderUp.add(queue[levelToGo]);
                     System.out.printf("Подобрал человека на %d этаже%n", i + 1);
                     System.out.printf("Принял команду перемещения на %d этаж%n", levelToGo + 1);
                 }
@@ -17,12 +27,16 @@ public class Lift {
             int[] queue = queues[i];
             for (int levelToGo : queue) {
                 if (levelToGo < i) {
+                    orderDown.add(i);
+                    orderDown.add(queue[levelToGo]);
                     System.out.printf("Подобрал человека на %d этаже%n", i + 1);
                     System.out.printf("Принял команду перемещения на %d этаж%n", levelToGo + 1);
                 }
             }
+
             System.out.printf("Двигаюсь на %d этаж%n", i);
         }
-        return new int[0];
+        orderDown.add(0);
+        return Stream.concat(orderUp.stream(), orderDown.descendingSet().stream()).mapToInt(Integer::intValue).toArray();
     }
 }
