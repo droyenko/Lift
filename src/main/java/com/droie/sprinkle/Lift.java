299c3348a76d9b9ecec2c0df70163fb0a5716bc7
Dmytro Roienko <droie@softserveinc.com>
2018-12-04 14:46:39 +0200

    basic functionality for Lift interview task

---
+++ src/main/java/com/droie/sprinkle/Lift.java
@@ -0,0 +1,27 @@
+package com.droie.sprinkle;
+
+public class Lift {
+    public static void theLift(final int[][] queues) {
+        for (int i = 0; i < queues.length; i++) {
+            int[] queue = queues[i];
+            for (int levelToGo : queue) {
+                if (levelToGo > i) {
+                    System.out.printf("Подобрал человека на %d этаже%n", i + 1);
+                    System.out.printf("Принял команду перемещения на %d этаж%n", levelToGo + 1);
+                }
+            }
+            System.out.printf("Двигаюсь на %d этаж%n", i + 2);
+        }
+
+        for (int i = queues.length - 1; i > 0; i--) {
+            int[] queue = queues[i];
+            for (int levelToGo : queue) {
+                if (levelToGo < i) {
+                    System.out.printf("Подобрал человека на %d этаже%n", i + 1);
+                    System.out.printf("Принял команду перемещения на %d этаж%n", levelToGo + 1);
+                }
+            }
+            System.out.printf("Двигаюсь на %d этаж%n", i);
+        }
+    }
+}
---
+++ src/main/java/com/droie/sprinkle/Main.java
@@ -0,0 +1,25 @@
+package com.droie.sprinkle;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[][] queues1 = {
+                new int[]{3}, // 1
+                new int[0], // 2
+                new int[]{1}, // 3
+                new int[]{0}, // 4
+        };
+
+        int[][] queues2 = {
+                new int[0], // 1
+                new int[0], // 2
+                new int[]{5,5,5}, // 3
+                new int[0], // 4
+                new int[0], // 5
+                new int[0], // 6
+                new int[0], // 7
+        };
+        Lift.theLift(queues2);
+    }
+}
